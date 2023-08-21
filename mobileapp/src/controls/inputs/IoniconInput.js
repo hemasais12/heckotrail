c52b8a048b18b0cdb9b847f8e11fe63a54100d0d
Narendra <narendrakg@gmail.com>
2023-08-21 07:35:57 +0530

    vendor setup

--- mobileapp/src/controls/inputs/IoniconInput.js
+++ mobileapp/src/controls/inputs/IoniconInput.js
@@ -1,12 +1,83 @@
 import { View, Text, TextInput, StyleSheet } from "react-native";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import { GlobalColors } from "../../common/colors";
 
-function IoniconInput({ ioniconprops, inputprops, isInvalid }) {
+import { GlobalColors } from "../../common/colors";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import EvilIcons from "@expo/vector-icons/EvilIcons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import Entypo from "@expo/vector-icons/Entypo";
+
+function IoniconInput({ ioniconprops, ioniconType, inputprops, isInvalid }) {
+  function prepareIonicon() {
+    switch (ioniconType) {
+      case "Ionicons":
+        return (
+          <Ionicons
+            {...ioniconprops}
+            size={20}
+            color={GlobalColors.icons.color}
+          />
+        );
+      case "AntDesign":
+        return (
+          <AntDesign
+            {...ioniconprops}
+            size={20}
+            color={GlobalColors.icons.color}
+          />
+        );
+        break;
+      case "FontAwesome":
+        return (
+          <FontAwesome
+            {...ioniconprops}
+            size={20}
+            color={GlobalColors.icons.color}
+          />
+        );
+        break;
+      case "EvilIcons":
+        return (
+          <EvilIcons
+            {...ioniconprops}
+            size={20}
+            color={GlobalColors.icons.color}
+          />
+        );
+        break;
+      case "MaterialIcons":
+        return (
+          <MaterialIcons
+            {...ioniconprops}
+            size={20}
+            color={GlobalColors.icons.color}
+          />
+        );
+        break;
+      case "Entypo":
+        return (
+          <Entypo
+            {...ioniconprops}
+            size={20}
+            color={GlobalColors.icons.color}
+          />
+        );
+        break;
+      default:
+        return (
+          <Ionicons
+            {...ioniconprops}
+            size={20}
+            color={GlobalColors.icons.color}
+          />
+        );
+    }
+  }
+
   return (
     <View style={styles.inputContainer}>
-      <Ionicons {...ioniconprops} color={GlobalColors.icons.color} />
-
+      {prepareIonicon()}
       <TextInput
         {...inputprops}
         style={[styles.input, isInvalid && styles.invalid]}
@@ -20,7 +91,15 @@ export default IoniconInput;
 const styles = StyleSheet.create({
   inputContainer: {
     flexDirection: "row",
+    alignItems: "center",
+    marginTop: 12,
   },
   label: {},
   invalid: {},
+  input: {
+    flex: 1,
+    marginLeft: 8,
+    borderBottomColor: GlobalColors.input.borderColor,
+    borderBottomWidth: 1,
+  },
 });
--- mobileapp/src/screens/vendor/EditableVendorProfile.js
+++ mobileapp/src/screens/vendor/EditableVendorProfile.js
@@ -7,9 +7,47 @@ function EditableVendorProfile({ children }) {
   return (
     <ScreenBackground>
       <View style={styles.container}>
+        <View>
+          <Text> Screen Description</Text>
+        </View>
         <IoniconInput
-          ioniconprops={{ name: "person-outline", size: 20 }}
+          ioniconprops={{ name: "user" }}
+          ioniconType="AntDesign"
           inputprops={{ placeholder: ScreenTextEn.VendorProfile.name }}
+        />
+        <IoniconInput
+          ioniconprops={{ name: "mobile1" }}
+          ioniconType="AntDesign"
+          inputprops={{ placeholder: ScreenTextEn.General.contactNumber }}
+        />
+        <IoniconInput
+          ioniconprops={{ name: "address" }}
+          ioniconType="Entypo"
+          inputprops={{ placeholder: ScreenTextEn.General.addressLine1 }}
+        />
+        <IoniconInput
+          ioniconprops={{ name: "address" }}
+          ioniconType="Entypo"
+          inputprops={{ placeholder: ScreenTextEn.General.addressLine2 }}
+        />
+        <IoniconInput
+          ioniconprops={{ name: "address" }}
+          ioniconType="Entypo"
+          inputprops={{ placeholder: ScreenTextEn.General.addressLine3 }}
+        />
+        <IoniconInput
+          ioniconprops={{ name: "location-city" }}
+          ioniconType="MaterialIcons"
+          inputprops={{ placeholder: ScreenTextEn.General.city }}
+        />
+        <IoniconInput
+          ioniconprops={{ name: "location-outline" }}
+          inputprops={{ placeholder: ScreenTextEn.General.state }}
+        />
+        <IoniconInput
+          ioniconprops={{ name: "map-pin" }}
+          ioniconType="FontAwesome"
+          inputprops={{ placeholder: ScreenTextEn.General.pinCode }}
         />
       </View>
     </ScreenBackground>
@@ -21,6 +59,5 @@ export default EditableVendorProfile;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
   },
 });
