549972ecba4f16ebf5b9b7806f16bf9c8a160795
Narendra <narendrakg@gmail.com>
2023-08-26 23:04:24 +0530

    Update IConstants.java

--- src/main/java/com/narenkg/hecko/consts/IConstants.java
+++ src/main/java/com/narenkg/hecko/consts/IConstants.java
@@ -4,5 +4,5 @@ public interface IConstants {
 	int OTP_LENGTH = 6;
 	int REFERRAL_CODE_LENGTH = 8;
 	String APP_SENDER_MOBILENUMBER = "+12294664808";
-	String APP_SENDER_EMAIL = "narenkgcts@outlook.com";
+	String APP_SENDER_EMAIL = "heckoapp2@outlook.com";
 }
