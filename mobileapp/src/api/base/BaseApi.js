8624d205ea32acc6434b079a1e983ee53a318cb5
Narendra <narendrakg@gmail.com>
2023-08-26 07:37:11 +0530

    apis

---
+++ mobileapp/src/api/base/BaseApi.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { API_URL } from "../../constants";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { STORAGE_TOKEN } from "../../common/constants";
+
+export async function request(options) {
+  const headers = new Headers({
+    "Content-Type": "application/json",
+  });
+
+  const storedToken = await AsyncStorage.getItem(STORAGE_TOKEN);
+  if (storedToken) {
+    headers.append("Authorization", "Bearer " + storedToken);
+  }
+
+  const defaults = { headers: headers };
+  options = Object.assign({}, defaults, options);
+
+  return fetch(options.url, options).then((response) =>
+    response.json().then((json) => {
+      if (!response.ok) {
+        return Promise.reject(json);
+      }
+      return json;
+    })
+  );
+}
